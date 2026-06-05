98e172e5db234bda07a49a6cecb66c49ba87d637
Caio Romão <caiodev14@gmail.com>
2026-06-05 20:56:10 -0300

    feat: query do projeto finalizada

--- curso-sql/etl_projeto.sql
+++ curso-sql/etl_projeto.sql
@@ -1,16 +1,20 @@
+CREATE TABLE tb_feature_store_cliente AS
+
 WITH tb_transacoes AS (
     SELECT IdTransacao,
             IdCliente,
             qtdePontos,
             datetime(substr(DtCriacao, 1, 19)) AS dtCriacao,
-            julianday('now') - julianday(substr(DtCriacao, 1, 10)) AS diffDate
+            julianday('2025-06-01') - julianday(substr(DtCriacao, 1, 10)) AS diffDate,
+            CAST(strftime('%H', substr(DtCriacao, 1, 19)) AS INTEGER) AS dtTime
     FROM transacoes
+    WHERE DtCriacao < '2025-06-01'
 ),
 
 tb_cliente AS (
     SELECT idCliente,
             datetime(substr(DtCriacao, 1 , 19)) AS dtCriacao,
-            julianday('now') - julianday(substr(DtCriacao, 1, 10)) AS idadeBase 
+            julianday('2025-06-01') - julianday(substr(DtCriacao, 1, 10)) AS idadeBase 
     FROM clientes
 ),
 
@@ -85,6 +89,26 @@ tb_cliente_produto_rn AS (
         FROM tb_cliente_produto
 ),
 
+tb_cliente_dia AS (
+
+        SELECT  idCliente,
+                strftime('%w',DtCriacao) AS dtDia,
+                count(*) AS qtDeTransacao
+        FROM tb_transacoes
+        WHERE diffDate <= 28
+
+        GROUP BY idCliente, dtDia
+),
+
+tb_cliente_dia_rn AS (
+
+SELECT *,
+        row_number() OVER (PARTITION BY idCliente ORDER BY qtdeTransacao DESC) AS rnDia
+
+FROM tb_cliente_dia
+
+),
+
 tb_join AS (
 
         SELECT t1.*,
@@ -93,7 +117,9 @@ tb_join AS (
                 T4.DescNomeProduto AS produto56,
                 T5.DescNomeProduto AS produto28,
                 T6.DescNomeProduto AS produto14,
-                T7.DescNomeProduto AS produto7
+                T7.DescNomeProduto AS produto7,
+                coalesce(t8.dtDia, -1) AS dtDia,
+                coalesce(t9.periodo, 'SEM INFORMACAO') AS periodoMaisTransacao28
                 
         FROM tb_sumario_transacoes AS t1
 
@@ -118,10 +144,50 @@ tb_join AS (
 
         LEFT JOIN tb_cliente_produto_rn AS t7
         ON t1.idCliente = t7.idCliente
-        AND t6.rn7 = 1
+        AND t7.rn7 = 1
+
+        LEFT JOIN tb_cliente_dia_rn AS t8
+        ON t1.idCliente = t8.idCliente
+        AND t8.rnDia = 1
+
+        LEFT JOIN tb_cliente_periodo_rn AS T9
+        ON t1.idCliente = t9.idCliente
+        AND t9.rnPeriodo = 1
         
         ORDER BY idCliente
+),
+
+tb_cliente_periodo AS (
+
+        SELECT idCliente,
+                CASE 
+                        WHEN dtTime BETWEEN 7 AND 12 THEN 'MANHÃ'
+                        WHEN dtTime BETWEEN 13 AND 18 THEN 'TARDE'
+                        WHEN dtTime BETWEEN 19 AND 23 THEN 'NOITE'
+                        ELSE 'MADRUGADA'
+                END AS periodo,
+                count(*) AS qtDeTransacao
+
+        FROM tb_transacoes
+        WHERE diffDate <= 28
+        GROUP BY 1,2
+),
+
+tb_cliente_periodo_rn AS (
+
+        SELECT *,
+                row_number() OVER (PARTITION BY idCliente ORDER BY qtDeTransacao DESC) AS rnPeriodo
+        FROM tb_cliente_periodo
 )
 
+INSERT INTO tb_feature_store_cliente
+
+SELECT '2025-06-01' AS dtRef,  
+        *,
+        1. * qtdeTransacoesD28 / qtdeTransacoesVida AS engajamento28Vida
+FROM tb_join   
+
+
 SELECT *
-FROM tb_join
+FROM tb_feature_store_cliente
+ORDER BY idCliente, dtRef       
